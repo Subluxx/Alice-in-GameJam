6f8f4e66be67e4b985e635dcba72d65984b78ec2
Ben <ben.a.osborn@gmail.com>
2024-08-17 20:53:05 +0100

    enlarge

--- Assets/Ben_Scripts/EventClick.cs
+++ Assets/Ben_Scripts/EventClick.cs
@@ -1,32 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class EventClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+public class EventClick : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
+    ReSizeRoom reSizeRoom;
+    PopUp popUp;
     Outline outline;
+    bool touching = false;
+    //ReSizeRoom reSizeRoom;
+    private Vector2 scale;
     private void Awake(){
+        reSizeRoom = GetComponent<ReSizeRoom>();
+        popUp = GetComponent<PopUp>();
         outline = gameObject.AddComponent<Outline>();
         outline.OutlineMode = Outline.Mode.OutlineHidden;
         outline.OutlineColor = Color.magenta;
         outline.OutlineWidth = 5f;
         //materialApplier = GetComponent<MaterailApplier>;
     }
+    public void OnPointerClick(PointerEventData eventData){
+        //Debug.Log("FRENCH TOAST");
+    }
     public void OnPointerDown(PointerEventData eventData){
-        //materialApplier.ApplyOther();
     }
     public void OnPointerUp(PointerEventData eventData){
-        //materialApplier.ApplyOriginal();
-    }
-    public void OnPointerClick(PointerEventData eventData){
-        Debug.Log("FRENCH TOAST");
     }
     public void OnPointerEnter(PointerEventData eventData){
         outline.OutlineMode = Outline.Mode.OutlineAll;
+        popUp.popUpVisable();
+        touching = true;
+
     }
     public void OnPointerExit(PointerEventData eventData){
+        popUp.popUpInvisable();
         outline.OutlineMode = Outline.Mode.OutlineHidden;
+        touching = false;
+    }
+
+    private void Update(){
+        if(touching){
+            if(Input.GetKey(KeyCode.F)){
+                reSizeRoom.ReSizeObject();
+            }
+        }
     }
 }
--- Assets/Ben_Scripts/ReSizeRoom.cs
+++ Assets/Ben_Scripts/ReSizeRoom.cs
@@ -1,18 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ReSizeRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+    private float cool = 1;
+    public void ReSizeObject(){
+            cool += Time.deltaTime;
+            transform.localScale = new Vector3(cool, cool, 1);
         
     }
 }
