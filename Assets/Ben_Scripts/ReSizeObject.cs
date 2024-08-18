f41caf7f84c7a4f1744dd8a52affeda202dd03a5
Ben <ben.a.osborn@gmail.com>
2024-08-18 20:25:59 +0100

    size limits

--- Assets/Ben_Scripts/ReSizeObject.cs
+++ Assets/Ben_Scripts/ReSizeObject.cs
@@ -1,18 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ReSizeObject : MonoBehaviour
 {
-    private float cool = 1;
+    private Vector3 startSize;
+    [SerializeField] private Vector3 endSize;
+    [SerializeField] private float duration;
+    private float elapsedTime;
+    void Awake(){
+        startSize = transform.localScale;
+        elapsedTime = transform.localScale.x;
+    }
     public void reSizeObject(){
-        cool += Time.deltaTime;
-        transform.localScale = new Vector3(cool, cool, 1);
+        // while(elapsedTime <= duration){
+        //     elapsedTime += Time.deltaTime;
+        //     //Debug.Log(elapsedTime);
+        //     float percentageComplete = elapsedTime/duration;
+        //     Debug.Log(percentageComplete);
+        //     transform.localScale = Vector3.Lerp(startSize, endSize, percentageComplete);
+
+        // }
+        if(transform.localScale.x<3f){
+            elapsedTime += Time.deltaTime;
+            transform.localScale = new Vector3(elapsedTime, elapsedTime, 1);
+        }
         
     }
     public void shrinkObject(){
-        cool -= Time.deltaTime;
-        transform.localScale = new Vector3(cool, cool, 1);
+        if(transform.localScale.x>1f){
+            elapsedTime -= Time.deltaTime;
+            transform.localScale = new Vector3(elapsedTime, elapsedTime, 1);
+        }
     }
+
 }
