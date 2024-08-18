5e7edb45cb98d0b5724877709345ccb8db5fafe4
Ben <ben.a.osborn@gmail.com>
2024-08-18 19:10:45 +0100

    polished

--- Assets/Ben_Scripts/BigRoom.cs
+++ Assets/Ben_Scripts/BigRoom.cs
@@ -6,21 +6,34 @@ public class BigRoom : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject[] otherRooms;
+    private GameObject[] placeHolder;
+    [SerializeField] private float scaleFactor;
+    private int count = -1;
     void Awake(){
-        otherRooms = GameObject.FindGameObjectsWithTag("Room");
+        otherRooms = new GameObject[5];
+        placeHolder = GameObject.FindGameObjectsWithTag("Room");
+        foreach(GameObject i in placeHolder){
+            if(i.transform.position != transform.position){
+                count++;
+                otherRooms.SetValue(value: i, index: count);
+                //otherRooms[count] = i;
+            }
+        }
     }
     public void enlarge(){
-        // foreach(Transform childTransfrom in this.transform){
-        //     Transform w = childTransfrom.GetComponent<Transform>();
-        //     w.localScale = new Vector3(w.localScale.x*2, w.localScale.y*2,1);
-        // }
-        transform.localScale = new Vector3(transform.localScale.x*1.5f, transform.localScale.y*1.5f,1);
-        foreach(GameObject i in otherRooms){
-            i.transform.localScale = new Vector3(i.transform.localScale.x/1.5f, i.transform.localScale.y/1.5f,1);
+        if(transform.localScale.y == 1){
+            transform.localScale = new Vector3(transform.localScale.x*scaleFactor, transform.localScale.y*scaleFactor,1);
+            foreach(GameObject i in otherRooms){
+                i.transform.localScale = new Vector3(i.transform.localScale.x/scaleFactor, i.transform.localScale.y/scaleFactor,1);
+            }
         }
-        //otherRoom.transform.localScale = new Vector3(otherRoom.transform.localScale.x/1.5f, otherRoom.transform.localScale.y/1.5f,1);
     }
-    // void OnTriggerEnter(){
-    //     transform.position = new Vector3(transform.position.x+0.5f,transform.position.y,1);
-    // }
+    public void returnSize(){
+        if(transform.localScale.y != 1){
+            transform.localScale = new Vector3(transform.localScale.x/scaleFactor,transform.localScale.y/scaleFactor,1);
+            foreach(GameObject i in otherRooms){
+                    i.transform.localScale = new Vector3(i.transform.localScale.x*scaleFactor, i.transform.localScale.y*scaleFactor,1);
+            }
+        }
+    }
 }
